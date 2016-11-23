42a2c91395b73650457cee27da42b048734ddf2a
kizs <kizsdevelop@gmail.com>
2016-11-23 20:29:13 +0100

    .

---
+++ releases/release.sh
@@ -0,0 +1,7 @@
+zip -r plugin.video.huncoretv-2.0.zip plugin.video.huncoretv
+#md5sum -b plugin.video.huncoretv-1.5.zip > plugin.video.huncoretv-1.5.md5
+zip -r plugin.video.bithumentv-2.0.zip plugin.video.bithumentv
+zip -r service.kizstorrent-1.0.zip service.kizstorrent
+#md5sum -b plugin.video.bithumentv-1.4.zip > plugin.video.bithumentv-1.4.md5
+#zip -r plugin.video.animeaddicts-1.1.zip plugin.video.animeaddicts
+#md5sum -b plugin.video.animeaddicts-1.0.zip > plugin.video.animeaddicts-1.0.md5
